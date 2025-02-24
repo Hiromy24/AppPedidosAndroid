bed863ef07230bb62b014defa819f011ef7ba369
Arpad Kiss <kissarpico22@gmail.com>
2025-02-24 19:47:08 +0100

    update todo

---
+++ app/src/main/java/com/example/apppedidosandroid/AddressesActivity.java
@@ -0,0 +1,28 @@
+package com.example.apppedidosandroid;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class AddressesActivity extends AppCompatActivity {
+
+    MaterialToolbar topAppBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_addresses);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
