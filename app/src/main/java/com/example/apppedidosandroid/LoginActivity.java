6473d15d0f6945541dd385a980cb9f3ddea30e83
Arpad Kiss <kissarpico22@gmail.com>
2025-02-28 20:25:52 +0100

    add: register func

--- app/src/main/java/com/example/apppedidosandroid/LoginActivity.java
+++ app/src/main/java/com/example/apppedidosandroid/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.apppedidosandroid;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -64,11 +66,15 @@ public class LoginActivity extends AppCompatActivity {
                         Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
                         try {
                             GoogleSignInAccount account = task.getResult(ApiException.class);
-                            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                            AuthCredential credential = GoogleAuthProvider
+                                    .getCredential(account.getIdToken(), null);
                             FirebaseAuth.getInstance().signInWithCredential(credential)
                                     .addOnCompleteListener(this, task1 -> {
                                         if (task1.isSuccessful()) {
-                                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                            FirebaseUser user = FirebaseAuth.getInstance()
+                                                    .getCurrentUser();
+                                            Toast.makeText(this, "Google sign in successful",
+                                                    Toast.LENGTH_SHORT).show();
                                             if (user != null) {
                                                 SharedPreferences prefs = getSharedPreferences(getString(R.string.prefs_file), MODE_PRIVATE);
                                                 SharedPreferences.Editor editor = prefs.edit();
@@ -94,27 +100,41 @@ public class LoginActivity extends AppCompatActivity {
         //endregion
 
         btnRegister.setOnClickListener(v -> {
-
-            /*if (!etEmail.getText().toString().isEmpty() &&
-                    !etPassword.getText().toString().isEmpty()) {
+            if (!etEmail.getText().toString().isEmpty() && !etPassword.getText().toString().isEmpty()) {
                 FirebaseAuth.getInstance()
-                        .createUserWithEmailAndPassword
-                                (etEmail.getText().toString(), etPassword.getText().toString())
+                        .createUserWithEmailAndPassword(etEmail.getText().toString(),
+                                etPassword.getText().toString())
                         .addOnCompleteListener(task -> {
                             if (task.isSuccessful()) {
+                                Toast.makeText(this, "Register completed",
+                                        Toast.LENGTH_SHORT).show();
 
+                                AlertDialog dialog = new android.app.AlertDialog.Builder(this)
+                                        .setTitle("Register successful")
+                                        .setMessage("Please, sign in with your new account")
+                                        .setPositiveButton("Accept", (dialogInterface, which) -> {
+                                            dialogInterface.dismiss();
+                                        })
+                                        .setOnDismissListener(dialogInterface -> {
+                                            Intent intent = new Intent();
+                                            setResult(RESULT_OK, intent);
+                                            finish();
+                                        })
+                                        .create();
 
+                                dialog.setOnShowListener(dialogInterface -> {
+                                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                                            .setTextColor(ContextCompat
+                                                    .getColor(this, android.R.color.black));
+                                });
 
-                                Intent intent = new Intent();
-                                setResult(RESULT_OK, intent);
-                                finish();
+                                dialog.show();
                             } else {
-                                Toast.makeText(this, "Error on registration",
+                                Toast.makeText(this, "Error on register",
                                         Toast.LENGTH_SHORT).show();
                             }
                         });
-
-            }*/
+            }
         });
 
         btnLogin.setOnClickListener(k -> {
